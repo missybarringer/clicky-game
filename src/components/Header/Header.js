a41ca7d80fac709885fd5025d6a606691a343e0a
GoatFeatherFarm <44263291+missybarringer@users.noreply.github.com>
2019-06-04 14:03:02 -0500

    visual changes & cleanup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Header.css";
 
 const Header = props => (
+    <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light">
     <div className="header">
         <div className="App-logo"></div>
         <div className="title">{props.children}</div>
@@ -9,6 +10,7 @@ const Header = props => (
             Score: {props.score} Highscore: {props.highscore}
         </div>
     </div>
+    </nav>
 );
 
 export default Header;
